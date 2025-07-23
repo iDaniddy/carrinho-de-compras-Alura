92e83f1583ac3958d4528c51f65ca625da0aa9ab
Daniel Dêividy <daniel.deividy@outlook.com>
2025-07-22 22:26:19 -0300

    secund commit

--- js/app.js
+++ js/app.js
@@ -1,29 +1,48 @@
-let totalGeral = 0;
+let totalGeral;
 limpar();
 
-function adicionar(){
-    //recuperar valores nome do produto, quantidade e valor
+
+function adicionar() {
     let produto = document.getElementById('produto').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
-    //calcular o preço, o nosso subtotal
+
+
+    // Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
+
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = parseFloat(produto.split('R$')[1]);
     let preco = quantidade * valorUnitario;
+
+
     let carrinho = document.getElementById('lista-produtos');
-    //adicionar no carrinho
-    carrinho.innerHTML = carrinho.innerHTML + ` <section class="carrinho__produtos__produto">
-        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-        </section>`;
-    //atualizar o valor total
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
+
+
     totalGeral = totalGeral + preco;
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
-    document.getElementById('quantidade').value = '';
-
+    document.getElementById('quantidade').value = 0;
 }
 
-function limpar(){
+
+
+
+function limpar() {
     totalGeral = 0;
     document.getElementById('lista-produtos').innerHTML = '';
-    document.getElementById('valor-total').textContent = 'R$ 00';
+    document.getElementById('valor-total').textContent = 'R$0';
 }
